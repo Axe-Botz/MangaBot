a9c6a273c71792e3c4ffae08599c5367cb7ccce7
Axe-Botz <136898915+Axe-Botz@users.noreply.github.com>
2023-06-22 17:02:56 +0530

    Update __main__.py

--- Asura/__main__.py
+++ Asura/__main__.py
@@ -383,7 +383,24 @@ def download_images(image_urls):
     return image_paths
 
 
+@asura.on_message(filters.command("start"))
+def start(_, message: Message):
+    message.reply_text("Welcome to AnimePFPBot! Send /get_pfp to get a random anime character profile picture.")
 
+# Handler function for the /get_pfp command
+@asura.on_message(filters.command("get_pfp"))
+def get_pfp(_, message: Message):
+    # Fetch a random anime pfp from a list of URLs
+    anime_pfps = [
+        "https://api.myanimelist.net/v2",
+        # Add more URLs here
+    ]
+    
+    # Select a random pfp URL
+    pfp_url = random.choice(anime_pfps)
+    
+    # Send the pfp to the user
+    message.reply_photo(photo=pfp_url)
 
 
 
